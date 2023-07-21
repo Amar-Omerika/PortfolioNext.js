477bb0e5163c8b04c726e3a60bb544eb644ed91b
Amar-Omerika <72925790+Amar-Omerika@users.noreply.github.com>
2023-07-21 13:53:21 +0200

    Added download cv functionallity

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -55,7 +55,7 @@ export default function Projects() {
         />
       </Head>
       <SimpleLayout
-        title="Things I’ve made trying to put my dent in the universe."
+        title="Things that I have made have an impact on our future."
         intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
       >
         <ul
